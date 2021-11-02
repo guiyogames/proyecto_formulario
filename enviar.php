684dbfdc4a8e8aca58f797618315cdb2dcd320b9
guiyogames <90001489+guiyogames@users.noreply.github.com>
2021-11-02 20:18:19 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -15,7 +15,7 @@ $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'argentina_02@hotmail.com';
+$para = 'giyogames@gmail.com';
 $asunto = 'Mensaje de prueba para proyecto';
 
 mail($para, $asunto, utf8_decode($message), $header);
